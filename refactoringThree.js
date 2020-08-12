3a3f85c91161e9d3a34f9ebe3b2380459c92c904
poem <normamoras@gmail.com>
2020-08-12 11:16:27 -0400

    function should do what they say

---
+++ refactoringThree.js
@@ -0,0 +1,17 @@
+//functionName should say what they do
+
+//before
+function addToDate(date, month){
+
+}
+
+const date = new Date();
+addToDate(date, 1);
+
+//after
+const addMonthToDate = (month, date) =>{
+
+}
+
+const date = new Date();
+addToDate(1, date)
